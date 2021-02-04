800532b1aa32979d1aa155f1f985862072dd1ba2
Berke <berkealm@gmail.com>
2021-02-05 00:02:01 +0300

    4.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -20,6 +20,9 @@ namespace ConsoleUI
             carManager.Update(new Car { Id = 4, BrandId = 2, ColorId = 4, DailyPrice = 7000, Description = "bmw3", ModelYear = 2000 });
             carManager.Delete(new Car { Id = 2, BrandId = 4, ColorId = 3, DailyPrice = 450, Description = "audi", ModelYear = 2014 });
             carManager.Update(new Car { Id = 3, BrandId = 5, ColorId = 1, DailyPrice = 755, Description = "volkswagen", ModelYear = 2010 });
+            //DAILY PRICE = 0 -ERROR- carManager.Add(new Car { Id = 5, BrandId = 7, ColorId = 4, DailyPrice = 0, Description = "slefusz", ModelYear = 2000 });
+            //DESCRIPTION.LENGHT < 2 -ERROR-carManager.Add(new Car { Id = 5, BrandId = 2, ColorId = 4, DailyPrice = 452, Description = "a", ModelYear = 1982 });
+
 
             foreach (var car in carManager.GetAllByBrandId(2))
             {
